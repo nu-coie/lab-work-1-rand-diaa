49920977c14a2824c88188fdb3c91a71b5515605
rand-diaa <130349080+rand-diaa@users.noreply.github.com>
2023-04-16 20:54:35 +0000

    ddd, Apr 4/16/2023, YYYY, 8:54 A +0

--- pyramid.c
+++ pyramid.c
@@ -10,29 +10,6 @@ int main(void)
 {
 
     int size;
-    do
-    {
-        size = get_int("Size: ");
-    } while (size < 1 || size > 8);
+    
 
-
-    for (int i = 0; i < size; i++)
-    {
-
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            printf(" ");
-        }
-
-
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-
-
-        printf("\n");
-    }
-
-    return 0; 
 }
