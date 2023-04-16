2893500fc8df223eea30037056375315a81cf1e2
rand-diaa <130349080+rand-diaa@users.noreply.github.com>
2023-04-16 20:49:29 +0000

    ddd, Apr 4/16/2023, YYYY, 8:49 A +0

--- pyramid.c
+++ pyramid.c
@@ -3,26 +3,36 @@
 
 int main(void)
 {
-    // Step ( 1 ) : Prompt the user for Pyramid Size
+    #include <stdio.h>
+#include <cs50.h>
 
-    
+int main(void)
+{
+    // Step (1): Prompt the user for Pyramid Size
+    int size;
+    do
+    {
+        size = get_int("Size: ");
+    } while (size < 1 || size > 8);
 
-    // Step ( 2 ) : Draw the Pyramid (Hint* : Use nested for loop)
-     
+    // Step (2): Draw the Pyramid
+    for (int i = 0; i < size; i++)
+    {
+        // Draw the spaces before the hashes
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        // Draw the hashes
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+
+        // Move to the next line
+        printf("\n");
+    }
+
+    return 0; // Exit program with success status
 }
-
-
-
-
-/*
-
-This is what the output should look like. :
-./pyramid
-Size: 4
-   #
-  ##
- ###
-####
-
-
-*/
