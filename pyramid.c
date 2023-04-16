8a136402e458a493d5f7153f7ccce269382c9cdf
rand-diaa <130349080+rand-diaa@users.noreply.github.com>
2023-04-16 20:53:14 +0000

    ddd, Apr 4/16/2023, YYYY, 8:53 A +0

--- pyramid.c
+++ pyramid.c
@@ -8,31 +8,31 @@ int main(void)
 
 int main(void)
 {
-    // Step (1): Prompt the user for Pyramid Size
+
     int size;
     do
     {
         size = get_int("Size: ");
     } while (size < 1 || size > 8);
 
-    // Step (2): Draw the Pyramid
+
     for (int i = 0; i < size; i++)
     {
-        // Draw the spaces before the hashes
+
         for (int j = 0; j < size - i - 1; j++)
         {
             printf(" ");
         }
 
-        // Draw the hashes
+
         for (int j = 0; j < i + 1; j++)
         {
             printf("#");
         }
 
-        // Move to the next line
+
         printf("\n");
     }
 
-    return 0; // Exit program with success status
+    return 0; 
 }
